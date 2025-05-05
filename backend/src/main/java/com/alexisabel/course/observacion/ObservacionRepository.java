3c48f9605e4c71e951d6f817c7ff2db8da8f9890
alexisabels <isabelsanchezalex@gmail.com>
2025-05-05 16:51:46 +0200

    Implementación de observaciones para las auditorías

---
+++ backend/src/main/java/com/alexisabel/course/observacion/ObservacionRepository.java
@@ -0,0 +1,8 @@
+package com.alexisabel.course.observacion;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ObservacionRepository extends JpaRepository<Observacion, Long> {
+}
---
+++ backend/src/main/java/com/alexisabel/course/observacion/ObservacionService.java
@@ -0,0 +1,70 @@
+package com.alexisabel.course.observacion;
+
+import com.alexisabel.course.auditoria.Auditoria;
+import com.alexisabel.course.auditoria.AuditoriaRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class ObservacionService {
+
+    private final ObservacionRepository observacionRepository;
+    private final AuditoriaRepository auditoriaRepository;
+
+    @Autowired
+    public ObservacionService(ObservacionRepository observacionRepository, AuditoriaRepository auditoriaRepository) {
+        this.observacionRepository = observacionRepository;
+        this.auditoriaRepository = auditoriaRepository;
+    }
+
+    public List<Observacion> getObservaciones() {
+        return observacionRepository.findAll();
+    }
+
+    public Optional<Observacion> getObservacion(Long observacionId) {
+        return observacionRepository.findById(observacionId);
+    }
+
+    public void addObservacion(ObservacionDTO dto) {
+        Auditoria auditoria = auditoriaRepository.findById(dto.getAuditoriaId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "La auditoría con id: " + dto.getAuditoriaId() + " no existe."));
+        Observacion observacion = new Observacion(dto.getDescripcion(), dto.getCriticidad(), auditoria);
+        observacionRepository.save(observacion);
+    }
+
+
+    @Transactional
+    public void updateObservacion(Long observacionId, ObservacionDTO dto) {
+        Observacion observacion = observacionRepository.findById(observacionId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "La observación con id: " + observacionId + " no existe."));
+
+        if (dto.getDescripcion() != null && !dto.getDescripcion().equals(observacion.getDescripcion())) {
+            observacion.setDescripcion(dto.getDescripcion());
+        }
+
+        if (dto.getCriticidad() != null && !dto.getCriticidad().equals(observacion.getCriticidad())) {
+            observacion.setCriticidad(dto.getCriticidad());
+        }
+
+        if (dto.getAuditoriaId() != null) {
+            Auditoria auditoria = auditoriaRepository.findById(dto.getAuditoriaId())
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "La auditoría con id: " + dto.getAuditoriaId() + " no existe."));
+            observacion.setAuditoria(auditoria);
+        }
+    }
+
+    public void deleteObservacion(Long observacionId) {
+        boolean exists = observacionRepository.existsById(observacionId);
+        if (!exists) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "La observacion con el id " + observacionId + " no existe");
+        }
+        observacionRepository.deleteById(observacionId);
+    }
+}
